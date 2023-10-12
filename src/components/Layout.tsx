e37f7fb872f13aa4c110f45128bcbdf12606257b
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-13 01:19:42 +0300

    make navbuttons component have prop for text in the button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
-import { Header } from "./Header";
-import { Navbar } from "./Navbar";
-import { Outlet } from "react-router-dom";
+
+import { Header } from './Header';
+import { Navbar } from './Navbar';
+import { Outlet } from 'react-router-dom';
 
 interface LayoutProps {
   showNavbar?: boolean;
