e03063a87c8f6aec8a90a8a5a83f5fe4bf267625
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-15 18:44:56 +0300

    add in pics for iTAK quickstart & atak datasync
    And start doing iTAK quickstart guide according to MikkoH's doc

--- src/views/servicetak/quickstartguide/ios/TakQuickstartIos1.tsx
+++ src/views/servicetak/quickstartguide/ios/TakQuickstartIos1.tsx
@@ -11,42 +11,31 @@ export function TakQuickstartIos1() {
   return (
     <div className="pb-32">
     <Layout showNavbar={true} showFooter={false} navbarTitle="iTAK Quickstart" backUrl="/app/services/tak/quickstart">
-    <StatusBar title="Asenna sovellukset" progressMax={5} progressNow={1} />
+    <StatusBar title="Asenna sovellukset (1/3)" progressMax={5} progressNow={1} />
     <div className="flex justify-center items-center w-full">
     <CardsContainer>
       
       <GuideSection
           number="1."
-          description='Lataa ATAK-CIV sovellus Play Storesta ja asenna se.
+          description='Lataa sovellus <strong>iTAK</strong> App Storesta ja asenna se.
           
-          <a href="https://play.google.com/store/apps/details?id=com.atakmap.app.civ" style="color: SteelBlue; text-decoration: underline;">ATAK-CIV - Paina tästä ladataksesi Google Playsta</a>
+          <a href="https://apps.apple.com/us/app/itak/id1561656396" style="color: SteelBlue; text-decoration: underline;">iTAK - Paina tästä ladataksesi App Storesta</a>
           <br></br>'
-      
-          description2='Tämän jälkeen lataa ATAK Data Sync-plugin, ja asenna se.
-         
-          <a href="https://play.google.com/store/apps/details?id=com.atakmap.android.datasync.plugin" style="color: SteelBlue; text-decoration: underline;">ATAK Datasync Plugin - Paina tästä ladataksesi Google Playsta</a>
-          <br></br>'
+    
 
-          description3="Olet nyt ladannut tarvittavat sovellukset."
+          description3="Olet nyt ladannut tarvittavan sovelluksen."
         />
         <GuideSection
           number="2."
           description="Lataa viestiperusteesi Palvelut/TAK-sivulta. Tallenna .zip-tiedosto."
           note="HUOMAA! Tiedostoa ei saa purkaa!"
         />
-        <GuideSection
-          number="3."
-          description="Avaa sovellus. Anna kaikki oikeudet, mitä applikaatio pyytää."
-          imageSrc={wait}         
-          imageClasses='m-3 w-[200px]'
-          description2='<em>Jos applikaatio antaa virheilmoituksen, valitse Wait. </em>'     
-        />
       
     </CardsContainer>
     </div>
       <NavigateButtons 
         backUrl="/app/services/tak/quickstart" 
-        forwardUrl="/app/services/tak/quickstart/android2" 
+        forwardUrl="/app/services/tak/quickstart/ios2" 
       />
 
     
