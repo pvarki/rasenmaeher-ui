06701ed0657c29262eee80247abfcb11a8a03330
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-16 00:14:50 +0300

    Do Wintak quickstart guide page2

--- src/views/servicetak/quickstartguide/wintak/TakQuickstartWin2.tsx
+++ src/views/servicetak/quickstartguide/wintak/TakQuickstartWin2.tsx
@@ -4,12 +4,12 @@ import wait from "../../../../assets/takguides/atak/wait.png"
 import { GuideSection} from "../../../../components/TakGuides/GuideSection";
 import { CardsContainer } from "../../../../components/CardsContainer";
 import { NavigateButtons } from "../../../../components/NavigateButtons";
-import pic1 from "../../../../assets/takguides/itak/itakquickstart1.png";
-import pic2 from "../../../../assets/takguides/itak/itakquickstart2.png";
-import pic3 from "../../../../assets/takguides/itak/itakquickstart3.png";
-import pic4 from "../../../../assets/takguides/itak/itakquickstart4.png";
-import pic5 from "../../../../assets/takguides/itak/itakquickstart5.png";
-import pic7 from "../../../../assets/takguides/itak/itakquickstart7.png";
+import pic9 from "../../../../assets/takguides/wintak/Kuva9.png";
+import pic10 from "../../../../assets/takguides/wintak/Kuva10.png";
+import pic11 from "../../../../assets/takguides/wintak/Kuva11.png";
+import pic12 from "../../../../assets/takguides/wintak/Kuva12.png";
+import pic13 from "../../../../assets/takguides/wintak/Kuva13.png";
+import pic14 from "../../../../assets/takguides/wintak/Kuva13-1.png";
 
 
 
@@ -19,72 +19,71 @@ export function TakQuickstartWin2() {
   return (
     <div className="pb-32">
     <Layout showNavbar={true} showFooter={false} navbarTitle="WinTAK pikaohje" backUrl="/app/services/tak/quickstart">
-    <StatusBar title="Tee perusasetukset" progressMax={4} progressNow={2} />
+    <StatusBar title="Lataa ja asenna WinTAK" progressMax={4} progressNow={2} />
     <div className="flex justify-center items-center w-full">
     <CardsContainer>
       
       <GuideSection
           number="1."
-          description='Avaa <strong>iTAK</strong>-sovellus.
+          description='Ladataan <strong>WinTAK</strong>-sovellus.
           <br></br>
-          Kun sovellus kysyy: <em>"iTAK haluaa etsiä lähiverkossa..."</em>, valitse <strong>Älä salli</strong>.'
-
-          imageSrc={pic1}
-          imageClasses="mx-auto pr-5 w-[240px] p-4"
+          WinTAK-sovelluksen käytettävä versio <strong> Puolustusvoimien osoittamasta paikasta</strong>.
+          <br></br>
+          <a href="https://arkipublic.blob.core.windows.net/ohjelmistot/WinTAK-CIV-latest.zip" style="color: SteelBlue; text-decoration: underline;">WinTAK - Lataa tästä WinTAK Puolustusvoimien jakelupisteestä.</a>
+          <br></br>'
+          note='Tärkeää! Lataa Windows-sovelluksia vain ohjeistetusta paikasta.'
+          description2='<small><em><br></br>WinTAK-sovelluksen toimittaja jakelee sovellusta tak.gov -sivustolta, mikä edellyttää kirjautumista. </em></small>'
         />
         <GuideSection
           number="2."
-          description='Täytä <strong>Profile Settings</strong>:'
+          description='Etsi latamaasi sovellus latauskansiosta.'
 
-          imageSrc={pic2}
-          imageClasses="mx-auto pr-5 w-[240px] p-4"
+          imageSrc={pic9}
+          imageClasses="mx-auto pr-5 w-[290px] p-4"
 
-          description2='Anna <strong>Callsign-kenttään</strong> oma peitteesi,
-          esimerkiksi <em>Rambo</em>.
-          <br></br>
-          Phone-kenttää <strong>ei täytetä</strong>
-          <br></br>
-          Paina lopuksi <strong>Next.</strong>'
+          description2='Avaa sovellus. Aukeaa asennusohjelma.'
       
         />
         <GuideSection
           number="3."
-          description='Täytä <strong>Team Settings:</strong>'
+          description='Asenna WinTAK käyttäen Setup Wizardia.'
 
-          imageSrc={pic3}
-          imageClasses="mx-auto pr-5 w-[240px] p-4"
+          imageSrc={pic10}
+          imageClasses="mx-auto pr-5 w-[300px] p-4"
 
-          description2='<ul>
-          Valitse <strong>Select Team</strong>-kenttään rullavalikosta joukkosi (käsketty) väri.
-          <br></br>
-           Valitse <strong>Select Team Role</strong>-kenttään rullavalikosta rooli <strong>Team Member</strong>, ellei muuta ole käsketty.
-          <br></br>
-          <small><em>Komentopaikkakäyttäjät asettavat roolin HQ.</em></small>
-          <br></br>
-          Paina lopuksi <strong>Next.</strong>
-          '
+          description2='Paina <strong>Next</strong>. Valitse sovelluksen asennuskansio.'
+
+          image2Src={pic11}
+          image2Classes="mx-auto pr-5 w-[300px] p-4"
+
+          description3='Vaihda halutessasi asennuskansiota <strong>Browse</strong>-napin kautta. Paina <strong>Next</strong>.'
+
         />
         <GuideSection
           number="4."
-          description='Aseta  <strong>Privacy Settings</strong> sallimaan ilmoitukset (notifications) ja sijainnin (location services.)'
+          description='Valitse Setup Wizardissa asennettavat lisäosat.
+          <br></br>
+          Valitse ainakin seuraavat lisäosat:
+          <ul>
+          <li>- <strong>Data Sync</strong> </li>
+          </ul>'
 
-          imageSrc={pic4}
-          imageClasses="mx-auto pr-5 w-[240px] p-4"
+          imageSrc={pic12}
+          imageClasses="mx-auto pr-5 w-[300px] p-4"
 
           description2='
-          iOS kysyy tämän jälkeen, sallitko iTAKin lähettävän sinulle <strong>ilmoituksia</strong> ja <strong>käyttää sijaintiasi</strong>.
+          Paina <strong>Next</strong>. Wizard on valmis aloittamaan asennuksen, paina <strong>Install</strong>. '
+
+          image2Src={pic13}
+          image2Classes="mx-auto pr-5 w-[300px] p-4"
+          description3='Paina <strong>Next</strong>.'
+
+          image3Src={pic14}
+          image3Classes="mx-auto pr-5 w-[300px] p-4"
+
+          description4='Sovellus on nyt asennettu.
           <br></br>
-          Valitse ilmoituksiin <strong>Salli</strong>.
-          '
-          image2Src={pic5}
-          image2Classes="mx-auto pr-5 w-[240px] p-4"
-
-          description3='Valitse sijaintiin <strong>Salli käytettäessä</strong>.'
-
-          image3Src={pic7}
-          image3Classes="mx-auto pr-5 w-[240px] p-4"
-
-          description4='Tämän jälkeen muodostetaan palvelinyhteys.'
+          Seuraavaksi avataan asennettu sovellus.'
         />
       
     </CardsContainer>
