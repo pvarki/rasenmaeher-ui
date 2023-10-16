bbd0357152a925c272da9c7c6bd9f1e8aaae66fd
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-16 12:02:08 +0300

    Alter heroimages to help user denote where we are

--- src/views/servicetak/quickstartguide/ServiceTakQuickstartView.tsx
+++ src/views/servicetak/quickstartguide/ServiceTakQuickstartView.tsx
@@ -1,24 +1,27 @@
 
 import android from "../../../assets/android.svg";
-import sanla from "../../../assets/heroimages/sanla.jpeg";
+import sanla from "../../../assets/heroimages/rakennettu3.jpeg";
 import apple from "../../../assets/apple.svg";
 import windows from "../../../assets/windows.svg";
-import { Text } from "../../../components/Text"
 import { Layout } from "../../../components/Layout"
 import { Card } from "../../../components/Card";
 import { CardsContainer } from "../../../components/CardsContainer";
-
+import { ServiceInfoCard } from "../../../components/ServiceInfoCard";
 
 
 export function ServiceTakQuickstartView() {
 
   return (
     <div className="pb-10">
-    <Layout showNavbar={true} showFooter={true} heroImage={sanla} navbarTitle="TAK: käyttöönotto-ohje" backUrl="/app">
-    <Text
-        title="Mitä laitetta käytät?"
-      />
+    <Layout showNavbar={true} showFooter={true} heroImage={sanla} navbarTitle="TAK: Käyttö joukossa" backUrl="/app">
+
     <CardsContainer>
+      <ServiceInfoCard
+        title="Ota TAK käyttöön!"
+        details="Valitse käyttöönotto-ohje laitteesi mukaan. Helpoin tapa tehdä käyttöönotto on <strong>parin kanssa</strong> siten, että toinen pari näyttää ohjetta ja toinen tekee."
+        >
+      </ServiceInfoCard>
+       
       <Card title="Android" image={android} url="/app/services/tak/quickstart/android1" />
       <Card title="iOS" image={apple} url="/app/services/tak/quickstart/ios1" />
       <Card title="Windows" image={windows} url="/app/services/tak/quickstart/win1" />
