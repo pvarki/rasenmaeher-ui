5ad2532f1caa14ff769566ac0a07337f0e839f67
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-17 15:58:03 +0300

    Refactor navigatebuttons to use buttons component
    DRY

--- src/components/NavigateButtons.tsx
+++ src/components/NavigateButtons.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "./Button";
 
 interface NavigateButtonsProps {
   backUrl: string;
@@ -27,28 +28,26 @@ export function NavigateButtons({ backUrl, forwardUrl, alterBack, alterForward }
 
   return (
     <div className="fixed bottom-2 left-0 right-0 flex justify-center">
-      <section className="items-start rounded bg-zinc-800 flex w-[370px] max-w-full gap-2.5 mt-24 p-2.5 max-md:justify-center">
-        <button
-          className={`transform transition-transform duration-200 justify-center items-center rounded ${BackClicked ? "scale-95" : "scale-100"} ${BackClicked ? "bg-blue-500" : "bg-zinc-800"} self-stretch flex w-[163px] max-w-full flex-col px-5 py-2.5`}
+      <section className="items-start rounded bg-zinc-900 flex w-[420px] min-h-[75px] max-w-full gap-2.5 mt-24 p-2.5 max-md:justify-center">
+        <Button
+          styling={`transform transition-transform duration-200 justify-center items-center rounded ${BackClicked ? "scale-95" : "scale-100"} self-stretch flex min-w-[150px] flex-col px-6 py-3 ${BackClicked ? "bg-blue-900" : "bg-zinc-800"}`}
           onMouseDown={() => setBackClicked(true)}
           onMouseUp={() => setBackClicked(false)}
           onClick={handleBackClick}
         >
-          <span className="text-white text-xl font-medium leading-6 self-center">
-            {alterBack || 'Takaisin'}
-          </span>
-        </button>
+          {alterBack || 'Takaisin'}
+        </Button>
+        
         <div className="self-stretch bg-neutral-800 flex w-1 h-11 flex-col" />
-        <button
-          className={`transform transition-transform duration-200 bg-blue-900 justify-center items-center ${ContinueClicked ? "scale-95" : "scale-100"} self-stretch flex w-[163px] max-w-full flex-col px-5 py-2.5`}
+
+        <Button
+          styling={`transform transition-transform duration-200 bg-blue-900 justify-center items-center ${ContinueClicked ? "scale-25" : "scale-100"} self-stretch flex min-w-[150px] flex-col px-6 py-3`}
           onMouseDown={() => setContinueClicked(true)}
           onMouseUp={() => setContinueClicked(false)}
           onClick={handleContinueClick}
         >
-          <span className="text-white text-xl font-medium leading-6 self-center">
-            {alterForward || 'Jatka'}
-          </span>
-        </button>
+          {alterForward || 'Jatka'}
+        </Button>
       </section>
     </div>
   );
