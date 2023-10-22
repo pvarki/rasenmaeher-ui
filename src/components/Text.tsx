30623c297bdcabebee191490b1b6bbcd70c66946
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-22 11:55:49 +0300

    minor styling & eslint

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,9 +6,7 @@ interface TextProps {
 export function Text(props: TextProps) {
   return (
     <div className="flex flex-col items-center p-0">
-      <h3 className="prose text-lg text-white font-bold mb-0">
-        {props.title}
-      </h3>
+      <h3 className="prose text-lg text-white font-bold mb-0">{props.title}</h3>
       <p className="prose text-white">{props.description}</p>
     </div>
   );
