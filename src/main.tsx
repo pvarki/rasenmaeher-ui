152a254d0754444c8d01ac333c131e3cf2eb8624
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-10-31 14:06:27 +0200

    Let's get rid of MSW and rather test against devmaeher

--- src/main.tsx
+++ src/main.tsx
@@ -6,30 +6,10 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-// MSW is only enabled in development mode
-declare const process: {
-  env: {
-    NODE_ENV: "development" | "production" | string;
-  };
-};
-
-async function deferRender(): Promise<void> {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
-  const { worker } = await import("./mocks/browser");
-  await worker.start({
-    onUnhandledRequest: "bypass",
-  });
-}
-
-void deferRender().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
