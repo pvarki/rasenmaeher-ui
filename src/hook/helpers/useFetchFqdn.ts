16e127c126d987cfc8c83e94fb7d7e94bebdcf1f
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-11-09 02:56:11 +0200

    my precommit checks must be funny somehow

--- src/hook/helpers/useFetchFqdn.ts
+++ src/hook/helpers/useFetchFqdn.ts
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useFetchFqdn = () => {
-  const [fqdn, setFqdn] = useState('');
+  const [fqdn, setFqdn] = useState("");
 
-interface HealthCheckResponse {
+  interface HealthCheckResponse {
     dns: string;
-}
+  }
 
-useEffect(() => {
-    fetch('/api/v1/healthcheck')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then((data: HealthCheckResponse) => {
-            setFqdn(data.dns);
-        })
-        .catch(error => {
-            console.error('Failed to fetch FQDN:', error);
-        });
-}, []);
+  useEffect(() => {
+    fetch("/api/v1/healthcheck")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data: HealthCheckResponse) => {
+        setFqdn(data.dns);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch FQDN:", error);
+      });
+  }, []);
 
   return fqdn;
 };
