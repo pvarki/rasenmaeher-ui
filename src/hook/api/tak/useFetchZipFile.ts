ebfd172d3f9792e7c14e6142d84f713e27a47e45
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-11-12 01:33:35 +0200

    fix ci

--- src/hook/api/tak/useFetchZipFile.ts
+++ src/hook/api/tak/useFetchZipFile.ts
@@ -27,18 +27,23 @@ async function fetchZipFile(): Promise<{ blob: Blob; filename: string }> {
   const data = (await res.json()) as FilesResponse;
   const fileData = data.files.tak[0]; // Assuming you want the first file in the 'tak' array
 
-  const byteCharacters = atob(fileData.data.split('base64,')[1]);
+  const byteCharacters = atob(fileData.data.split("base64,")[1]);
   const byteNumbers = new Array(byteCharacters.length);
   for (let i = 0; i < byteCharacters.length; i++) {
     byteNumbers[i] = byteCharacters.charCodeAt(i);
   }
   const byteArray = new Uint8Array(byteNumbers);
-  const blob = new Blob([byteArray], { type: 'application/zip' });
+  const blob = new Blob([byteArray], { type: "application/zip" });
 
   return { blob, filename: fileData.filename };
 }
 
-type UseFetchZipFileOptions = UseMutationOptions<{ blob: Blob; filename: string }, Error, void, unknown>;
+type UseFetchZipFileOptions = UseMutationOptions<
+  { blob: Blob; filename: string },
+  Error,
+  void,
+  unknown
+>;
 
 export function useFetchZipFile(options?: UseFetchZipFileOptions) {
   return useMutation(fetchZipFile, options);
