89b19a2345d6ec12573a961c1cf3107981f0076b
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-11-17 10:10:08 +0200

    Start doing sanity to unfoldablecard & refactor
    androidusageview1

---
+++ src/components/Step.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useAlertDialog } from "../components/AlertDialogService";
+
+export interface StepProps {
+    imageSrc?: string;
+    imageLink?: string;
+    imageClasses?: string;
+    description?: React.ReactNode;
+    note?: React.ReactNode;
+}
+
+export function Step({ 
+    imageSrc, imageLink, imageClasses, description, note }: StepProps): JSX.Element {
+    const { openDialog } = useAlertDialog();
+
+    const handleImageClick = (src: string, link: string) => {
+        if (link) {
+            window.location.href = link;
+        } else {
+            openDialog({
+                title: "Katso kuvaa suurempana",
+                description: (
+                    <img src={src} alt="Enlarged Content" style={{ maxWidth: "100%" }} />
+                ),
+                confirmLabel: "Sulje",
+                confirmColor: "primary",
+                onConfirm: () => {
+                    // just close the modal
+                },
+            });
+        }
+    };
+
+    const renderImage = (src = "", link = "", classes = "") =>
+        src && (
+            <img
+                src={src}
+                className={`mx-auto ${classes} cursor-pointer`}
+                alt="Unfoldable card content"
+                onClick={() => handleImageClick(src, link)}
+            />
+        );
+
+    return (
+        <>
+            {description && (
+                    <p className="m-2 text-white prose prose-white">{description}</p>
+                )}
+
+            {renderImage(imageSrc, imageLink, imageClasses)}
+        
+            {note && (
+                <div className="prose prose-white max-w-none text-red-400 mt-2">
+                    {note}
+                </div>
+            )}
+        </>
+    );
+}
--- src/views/servicetak/usage/android/TakUsageAndroid1.tsx
+++ src/views/servicetak/usage/android/TakUsageAndroid1.tsx
@@ -1,11 +1,11 @@
 import { Layout } from "../../../../components/Layout";
 import { StatusBar } from "../../../../components/StatusBar";
-import { GuideSection } from "../../../../components/TakGuides/GuideSection";
-import { UnfoldableCard } from "../../../../components/UnfoldableCard";
 import { CardsContainer } from "../../../../components/CardsContainer";
 import { NavigateButtons } from "../../../../components/NavigateButtons";
-import { ServiceTakUsageContents } from "../helpers/ServiceTakUsageContents";
-
+import { ServiceTakUsageCard } from "../helpers/ServiceTakUsageCard";
+import { UnfoldableCard } from "../../../../components/UnfoldableCard2";
+import { ServiceInfoCard } from "../../../../components/ServiceInfoCard";
+import ATAK from "../../../../assets/icons/tak-logo.png";
 import pic27 from "../../../../assets/takguides/atak/27-BaseMap.png";
 import pic28 from "../../../../assets/takguides/atak/28-Map.png";
 import frame1 from "../../../../assets/takguides/atak/Frame-1.png";
@@ -24,99 +24,127 @@ export function TakUsageAndroid1() {
         navbarTitle="ATAK: Käyttö joukossa"
         backUrl="/app/services/tak/Usage"
       >
-        <CardsContainer>
-          <UnfoldableCard
-            title="Sisältö"
-            styling="bg-background"
-            initialOpen={true}
-          >
-            <ServiceTakUsageContents />
-          </UnfoldableCard>
-        </CardsContainer>
         <StatusBar
-          title="Liikuta karttaa ja näe omat"
+          title="Liikuta karttaa ja näe omat (1/4)"
           progressMax={4}
           progressNow={1}
         />
 
         <CardsContainer>
-          <GuideSection
-            number="1."
-            description={
-              <span>
-                <strong>Navigaationapit</strong> ovat vasemmassa yläkulmassa:
-              </span>
-            }
-            imageSrc={pic27}
-            imageClasses="m-3 w-[300px]"
-          />
-          <GuideSection
-            number="2."
-            description={
-              <span>
-                Liikuta karttapohjaa <strong>sormella.</strong>
-              </span>
-            }
-            imageSrc={frame}
-            imageClasses="align-right w-[50px]"
-          />
-          <GuideSection
-            number="3."
-            description={
-              <span>
-                Zoomaa karttaa <strong>nipistysotteella.</strong>
-              </span>
-            }
-            imageSrc={frame1}
-            imageClasses="align-right w-[35px]"
-          />
-          <GuideSection
-            number="4."
-            description={
-              <span>
-                Käännä karttapohjaa <strong>kahdella sormella.</strong>
-              </span>
-            }
-            imageSrc={frame2}
-            imageClasses="align-right w-[50px]"
-          />
-          <GuideSection
-            number="5."
-            description={
-              <span>
-                Oma sijaintisi näkyy <strong>nuolenpäänä.</strong>
-              </span>
-            }
-            imageSrc={frame3}
-            imageClasses="align-right w-[40px]"
-          />
-          <GuideSection
-            number="6."
-            description={
-              <span>
-                Muiden käyttäjien peitenimet alkavat näkyä zoomatessa.
-              </span>
-            }
-            imageSrc={frame4}
-            imageClasses="align-right w-[50px]"
-          />
-          <GuideSection
-            number="7."
-            description={
-              <span>
-                Muiden käyttäjien pallokuvakkeen väri kuvaa joukkueen väriä (tai
-                muuta käskettyä merkitystä).
-              </span>
-            }
-            imageSrc={frame5}
-            imageClasses="align-right w-[50px]"
-            image2Src={pic28}
-            image2Classes="w-[300px]"
-          />
-          <NavigateButtons
-            backUrl="/app/services/tak/Usage"
-            forwardUrl="/app/services/tak/Usage/android2"
-          />
+          <div className="flex flex-col items-center w-full gap-2 justify-center p-5">
+            <ServiceInfoCard
+              title="ATAK"
+              image={ATAK}
+              details={
+                <>
+                  Käyttö joukossa ATAK - vaihe 1:{" "}
+                  <strong>liikuta karttaa ja näe omat</strong>
+                  <br />
+                </>
+              }
+            >
+              <ServiceTakUsageCard />
+            </ServiceInfoCard>
+            <UnfoldableCard
+              title="1. Liikuta karttaa"
+              styling="bg-backgroundLight"
+              steps={[
+                {
+                  description: (
+                    <>
+                      <strong>Navigaationapit</strong> ovat vasemmassa
+                      yläkulmassa:
+                    </>
+                  ),
+                  imageSrc: pic27,
+                  imageClasses: "m-3 w-[300px]",
+                },
+                {
+                  description: (
+                    <>
+                      <span>
+                        a. Liikuta karttapohjaa <strong>sormella.</strong>
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame,
+                  imageClasses: "align-right w-[50px]",
+                },
+                {
+                  description: (
+                    <>
+                      <span>
+                        b. Zoomaa karttaa <strong>nipistysotteella.</strong>
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame1,
+                  imageClasses: "align-right w-[35px]",
+                },
+                {
+                  description: (
+                    <>
+                      <span>
+                        c. Käännä karttapohjaa{" "}
+                        <strong>kahdella sormella.</strong>
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame2,
+                  imageClasses: "align-right w-[50px]",
+                },
+              ]}
+            />
+            <UnfoldableCard
+              title="2. Oma ja muiden sijainti"
+              styling="bg-backgroundLight"
+              steps={[
+                {
+                  description: (
+                    <>
+                      <span>
+                        a. Oma sijaintisi näkyy <strong>nuolenpäänä.</strong>
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame3,
+                  imageClasses: "align-right w-[40px]",
+                },
+                {
+                  description: (
+                    <>
+                      <span>
+                        b. Muiden käyttäjien peitenimet alkavat näkyä
+                        zoomatessa.
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame4,
+                  imageClasses: "align-right w-[50px]",
+                },
+                {
+                  description: (
+                    <>
+                      <span>
+                        c. Muiden käyttäjien pallokuvakkeen väri kuvaa joukkueen
+                        väriä (tai muuta käskettyä merkitystä).
+                      </span>
+                    </>
+                  ),
+                  imageSrc: frame5,
+                  imageClasses: "align-right w-[50px]",
+                },
+                {
+                  imageSrc: pic28,
+                  imageClasses: "w-[300px]",
+                },
+              ]}
+            />
+            <NavigateButtons
+              backUrl="/app/services/tak/Usage"
+              forwardUrl="/app/services/tak/Usage/android2"
+            />
+          </div>
         </CardsContainer>
       </Layout>
     </div>
