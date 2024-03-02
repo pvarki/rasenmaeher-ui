f4a6c61e716d1b1eec6bdebbd5ff1e50a643e15d
Benjam <53127823+benjamsf@users.noreply.github.com>
2024-03-02 15:37:02 +0200

    Add loadingcomponent to approve/decline enrolment
    For the convenience.

--- src/views/users/invite/EnrollApprovalView.tsx
+++ src/views/users/invite/EnrollApprovalView.tsx
@@ -17,6 +17,7 @@ import { useFormik } from "formik";
 import { useApproveUser } from "../../../hook/api/useApproveUser";
 import { useRejectUser } from "../../../hook/api/useRejectUser";
 import { useQueryClient } from "react-query";
+import LoadingComponent from "../../../components/Loading/LoadingComponent";
 
 interface UserDetails {
   callsign: string;
@@ -30,6 +31,8 @@ interface WaitingListAccordionProps {
 export function EnrollApprovalView() {
   const [isApproved, setIsApproved] = useState(false);
   const [isRejected, setIsRejected] = useState(false);
+  const [isRejecting, setIsRejecting] = useState(false);
+  const [isApproving, setIsApproving] = useState(false);
   const [rejectionMessage, setRejectionMessage] = useState("");
   const [confirmReject, setConfirmReject] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -40,14 +43,19 @@ export function EnrollApprovalView() {
   const location = useLocation();
 
   const { mutate: approveMutation, reset } = useApproveUser({
+    onMutate: () => {
+      setIsApproving(true);
+    },
     onSuccess: () => {
       setApprovalMessage("Käyttäjä hyväksytty.");
       setIsApproved(true);
+      setIsApproving(false);
       void queryClient.invalidateQueries(["enrollmentList"]);
     },
     onError: (error) => {
       setApprovalMessage(`Approval error: ${error.message}`);
       setIsError(true);
+      setIsApproving(false);
       console.error("Approval error:", error.message);
     },
   });
@@ -67,9 +75,23 @@ export function EnrollApprovalView() {
 
   const handleReject = () => {
     if (selectedUser) {
-      void rejectMutation({ callsign: selectedUser.callsign });
+      setIsRejecting(true);
+
+      rejectMutation(
+        { callsign: selectedUser.callsign },
+        {
+          onSuccess: () => {
+            setIsRejecting(false);
+            setIsRejected(true);
+            setDialogOpen(false);
+          },
+          onError: () => {
+            setIsRejecting(false);
+          },
+        },
+      );
+      setConfirmReject(false);
     }
-    setConfirmReject(false);
   };
 
   const query = useMemo(() => new URLSearchParams(location.search), [location]);
@@ -109,6 +131,11 @@ export function EnrollApprovalView() {
       openModal(user);
       void formik.setFieldValue("approvalCode", approvalCode);
     }
+    return () => {
+      closeModal();
+      setIsApproved(false);
+      setSelectedUser(null);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Empty dependency array ensures this runs once on mount
 
@@ -235,57 +262,79 @@ export function EnrollApprovalView() {
           <Dialog.Overlay className="bg-black opacity-60 data-[state=open]:animate-overlayShow fixed inset-0" />
           <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-background p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
             <Dialog.Title className="text-white m-0 text-lg text-bold font-medium">
-              Hyväksy käyttäjä
+              {isApproved ? "Käyttäjä hyväksytty" : "Hyväksy käyttäjä"}
             </Dialog.Title>
-            <Dialog.Description className="text-white mt-4 mb-5 text-md leading-normal">
-              {approvalMessage ||
-                "Syötä käyttäjän hyväksyntäkoodi ja paina Hyväksy."}
-            </Dialog.Description>
-            {!isApproved && !isError && (
-              <div className="text-white m-0 mb-4 text-md">
-                <p>
-                  <strong>Peitenimi:</strong> <br />
-                  {selectedUser?.callsign}
-                </p>
-                <form onSubmit={formik.handleSubmit}>
-                  <label htmlFor="approvalCode">
-                    <strong>Hyväksyntäkoodi:</strong>
-                  </label>
-                  <input
-                    id="approvalCode"
-                    name="approvalCode"
-                    type="text"
-                    onChange={formik.handleChange}
-                    value={formik.values.approvalCode}
-                    className="text-black font-consolas bg-gray-100 w-full p-2 rounded-lg"
-                  />
-                </form>
+            {isApproving ? (
+              <LoadingComponent text="Hyväksytään käyttäjää..." />
+            ) : isApproved ? (
+              <div className="text-white mt-4 mb-5 text-md">
+                Käyttäjä on hyväksytty.
+                <div className="mt-4 flex justify-end">
+                  <Button variant={{ color: "success" }} onClick={closeModal}>
+                    OK
+                  </Button>
+                </div>
               </div>
+            ) : (
+              <>
+                <Dialog.Description className="text-white mt-4 mb-5 text-md leading-normal">
+                  {approvalMessage ||
+                    "Syötä käyttäjän hyväksyntäkoodi ja paina Hyväksy."}
+                </Dialog.Description>
+                {!isError && (
+                  <div className="text-white m-0 mb-4 text-md">
+                    <p>
+                      <strong>Peitenimi:</strong> <br />
+                      {selectedUser?.callsign}
+                    </p>
+                    <form onSubmit={formik.handleSubmit}>
+                      <label htmlFor="approvalCode">
+                        <strong>Hyväksyntäkoodi:</strong>
+                      </label>
+                      <input
+                        id="approvalCode"
+                        name="approvalCode"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.approvalCode}
+                        className="text-black font-consolas bg-gray-100 w-full p-2 rounded-lg"
+                      />
+                    </form>
+                  </div>
+                )}
+                <div className="flex justify-end pt-6 gap-[10px]">
+                  <DialogButtons />
+                </div>
+              </>
             )}
-            <div className="flex justify-end pt-6 gap-[10px]">
-              <DialogButtons />
-            </div>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
       <Dialog.Root open={confirmReject} onOpenChange={setConfirmReject}>
         <Dialog.Portal>
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
           <Dialog.Content className="fixed top-1/2 left-1/2 w-full max-w-md p-8 bg-backgroundLight rounded-md transform -translate-x-1/2 -translate-y-1/2">
             <Dialog.Title className="text-lg text-white font-bold">
-              Oletko varma?
+              Hylkää käyttäjä
             </Dialog.Title>
-            <Dialog.Description className="mt-2 text-white">
-              Tämä toimintoa ei voi peruuttaa.
-            </Dialog.Description>
-            <div className="mt-4 flex justify-end gap-3">
-              <Dialog.Close asChild>
-                <Button variant={{ color: "tertiary" }}>Peruuta</Button>
-              </Dialog.Close>
-              <Button variant={{ color: "error" }} onClick={handleReject}>
-                Olen, hylkää
-              </Button>
-            </div>
+            {isRejecting ? (
+              <LoadingComponent text="Toimenpidettä suoritetaan..." />
+            ) : (
+              <>
+                <Dialog.Description className="mt-2 text-white">
+                  Oletko varma? Tämä toimintoa ei voi peruuttaa.
+                </Dialog.Description>
+                <div className="mt-4 flex justify-end gap-3">
+                  <Dialog.Close asChild>
+                    <Button variant={{ color: "tertiary" }}>Peruuta</Button>
+                  </Dialog.Close>
+                  <Button variant={{ color: "error" }} onClick={handleReject}>
+                    Hylkää
+                  </Button>
+                </div>
+              </>
+            )}
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
