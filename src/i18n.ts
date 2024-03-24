fb80f31da65f5ea54e7940a7c1b66718df937405
Sander Kallio <sander.kallio@gmail.com>
2024-03-24 18:24:36 +0200

    Add dynamic loading and caching to i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,29 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
+import ChainedBackend from "i18next-chained-backend";
+import LocalStorageBackend from "i18next-localstorage-backend";
 
-import translationFI from "./assets/locale/fi.json";
-import translationEN from "./assets/locale/en.json";
-
-i18n.use(initReactI18next).init({
-  resources: {
-    fi: {
-      translation: translationFI,
+i18n
+  .use(ChainedBackend)
+  .use(initReactI18next)
+  .init({
+    lng: "fi",
+    fallbackLng: "fi",
+    backend: {
+      backends: [
+        LocalStorageBackend,
+        resourcesToBackend(
+          async (lang: string) => import(`./assets/locale/${lang}.json`),
+        ),
+      ],
+      backendOptions: [
+        {
+          expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+        },
+      ],
     },
-    en: {
-      translation: translationEN,
+    interpolation: {
+      escapeValue: false,
     },
-  },
-  lng: "fi",
-  fallbackLng: "fi",
-
-  interpolation: {
-    escapeValue: false,
-  },
-});
+  });
