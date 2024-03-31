28b1509f50ea0ef8bdf4e4bf77f06dfbe9ae3a77
Benjam <53127823+benjamsf@users.noreply.github.com>
2024-03-31 17:41:42 +0300

    do i18n for takusageios (fi/en)

--- src/views/servicetak/usage/ios/TakUsageIos1.tsx
+++ src/views/servicetak/usage/ios/TakUsageIos1.tsx
@@ -5,6 +5,7 @@ import { UnfoldableCard } from "../../../../components/UnfoldableCard2";
 import { ServiceInfoCard } from "../../../../components/ServiceInfoCard";
 import { CardsContainer } from "../../../../components/CardsContainer";
 import { NavigateButtons } from "../../../../components/NavigateButtons";
+import { Trans, useTranslation } from "react-i18next";
 import pic26 from "../../../../assets/takguides/itak/itakusage1.png";
 import pic27 from "../../../../assets/takguides/itak/itakusage2.png";
 import pic271 from "../../../../assets/takguides/itak/itakusage31.png";
@@ -13,41 +14,44 @@ import pic29 from "../../../../assets/takguides/itak/itakusage5.png";
 import pic30 from "../../../../assets/takguides/itak/itakusage6.png";
 
 export function TakUsageIos1() {
+  const { t } = useTranslation();
   return (
     <div className="pb-20">
       <Layout
         showNavbar={true}
         showFooter={false}
-        navbarTitle="iTAK käyttö joukossa"
+        navbarTitle={t("TakUsageIos1.navbarTitle")}
         backUrl="/app/services/tak/usage"
       >
-        <StatusBar title="Lähetä merkkejä" progressMax={1} progressNow={1} />
+        <StatusBar
+          title={t("TakUsageIos1.statusBarTitle")}
+          progressMax={1}
+          progressNow={1}
+        />
         <CardsContainer>
           <div className="flex flex-col items-center w-full gap-2 justify-center p-5">
             <ServiceInfoCard
               title="iTAK"
               image={ITAK}
               details={
-                <>
-                  iTAK käyttö joukossa :
-                  <strong>Tee ja lähetä havaintoja</strong>
-                  <br />
-                </>
+                <Trans
+                  i18nKey="TakUsageIos1.serviceInfoCard.details"
+                  components={{ strong: <strong />, br: <br /> }}
+                />
               }
             />
             <UnfoldableCard
-              title="1. Luo merkki"
+              title={t("TakUsageIos1.unfoldableCard1.title")}
               styling="bg-backgroundLight"
               steps={[
                 {
-                  description: <>Voit luoda havaintomerkin seuraavasti.</>,
+                  description: (
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard1.step1" />
+                  ),
                 },
                 {
                   description: (
-                    <>
-                      Paina kohtaa, johon haluat merkin, kartalla pitkään.
-                      Syntyy merkki.
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard1.step2" />
                   ),
                   imageSrc: pic26,
                   imageClasses: "m-3 w-[400px]",
@@ -55,109 +59,90 @@ export function TakUsageIos1() {
               ]}
             />
             <UnfoldableCard
-              title="2. Täydennä tiedot"
+              title={t("TakUsageIos1.unfoldableCard2.title")}
               styling="bg-backgroundLight"
               steps={[
                 {
                   description: (
-                    <>
-                      Täydennä luomaasi merkkiin haluamaasi tietoa: mahdollisia{" "}
-                      <strong>liitteitä</strong> ja vähintään tietoa.
-                    </>
+                    <Trans
+                      i18nKey="TakUsageIos1.unfoldableCard2.step1"
+                      components={{ strong: <strong /> }}
+                    />
                   ),
                 },
                 {
                   description: (
-                    <>
-                      Painamalla <strong>Attachments</strong> voit lisätä mm.
-                      kuvia.
-                    </>
+                    <Trans
+                      i18nKey="TakUsageIos1.unfoldableCard2.step2"
+                      components={{ strong: <strong /> }}
+                    />
                   ),
                   imageSrc: pic27,
                   imageClasses: "m-3 w-[300px]",
                 },
                 {
                   description: (
-                    <>
-                      Painamalla <strong>Remarks</strong> voit lisätä merkinnät.
-                    </>
+                    <Trans
+                      i18nKey="TakUsageIos1.unfoldableCard2.step3"
+                      components={{ strong: <strong /> }}
+                    />
                   ),
                   imageSrc: pic271,
                   imageClasses: "m-3 w-[300px]",
-                  note: <>Tärkeää! Muista lisätä kuvaus merkkiin.</>,
+                },
+                {
+                  note: <Trans i18nKey="TakUsageIos1.unfoldableCard2.note" />,
                 },
               ]}
             />
-
             <UnfoldableCard
-              title="3. Lähetä merkki komentopaikalle"
+              title={t("TakUsageIos1.unfoldableCard3.title")}
               styling="bg-backgroundLight"
               steps={[
                 {
                   description: (
-                    <>
-                      Lähetä merkki komentopaikalla painamalla sen symbolia
-                      kartalla ensin pitkään.
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard3.step1" />
                   ),
                 },
                 {
                   description: (
-                    <>
-                      Aukeavassa näkymässä paina <strong>Share.</strong>
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard3.step2" />
                   ),
                   imageSrc: pic28,
                   imageClasses: "m-3 w-[400px]",
                 },
                 {
                   description: (
-                    <>
-                      Valitse <strong>Share to Contacts.</strong>
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard3.step3" />
                   ),
                   imageSrc: pic29,
                   imageClasses: "m-3 w-[400px]",
                 },
                 {
                   description: (
-                    <>
-                      Valitse Contacts-ruudusta (huom. kuvassa tyhjänä) sinulle
-                      käsketty <strong>komentopaikan tunnus</strong>, esim.
-                      KARHU1. Lähetä
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard3.step4" />
                   ),
                   imageSrc: pic30,
                   imageClasses: "m-3 w-[400px]",
-                  note: (
-                    <>
-                      Muista, että merkki ei ole koskaan riittävä ilmoitus.
-                      Lähetä lisäksi aina sanoma käsketyllä menetelmällä
-                      havainnostasi.
-                    </>
-                  ),
+                  note: <Trans i18nKey="TakUsageIos1.unfoldableCard3.note" />,
                 },
               ]}
             />
             <UnfoldableCard
-              title="4. Vastaanota merkkejä Recon Feedistä"
+              title={t("TakUsageIos1.unfoldableCard4.title")}
               styling="bg-backgroundLight"
               steps={[
                 {
                   description: (
-                    <>
-                      Jos olet käyttöönotto-ohjeen sivun 5 mukaisesti ottanut
-                      käyttöön Data Syncillä <strong>Recon Feedin</strong>,
-                      päivittyy sen sisältö automaattisesti kartallesi.
-                    </>
+                    <Trans
+                      i18nKey="TakUsageIos1.unfoldableCard4.step1"
+                      components={{ strong: <strong /> }}
+                    />
                   ),
                 },
                 {
                   description: (
-                    <>
-                      Tutki Recon Feedistä saamaasi tilannekuvaan kuuluvia
-                      merkkejä painamalla niitä.
-                    </>
+                    <Trans i18nKey="TakUsageIos1.unfoldableCard4.step2" />
                   ),
                   imageSrc: pic28,
                   imageClasses: "m-3 w-[400px]",
@@ -169,7 +154,7 @@ export function TakUsageIos1() {
         <NavigateButtons
           backUrl="/app/services/tak/usage"
           forwardUrl="/app/services/tak/usage"
-          alterForward="Valmis! Palaa tästä."
+          alterForward={t("TakUsageAndroid3.alterForward")}
         />
       </Layout>
     </div>
