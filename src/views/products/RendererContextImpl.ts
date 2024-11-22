84c6b3ef373d99dff5b62c9e84147ec6816eb9bc
Jaakko Heusala <jhh@hg.fi>
2024-11-22 18:00:37 +0200

    Refactored to use newer Rune implementation https://github.com/pvarki/rasenmaeher-ui/issues/10

--- src/views/products/RendererContextImpl.ts
+++ src/views/products/RendererContextImpl.ts
@@ -1,5 +1,8 @@
 import { NavigateFunction } from "react-router-dom";
 import { ProductContentService } from "./ProductContentService";
+import { BaseContent } from "./types/BaseContent";
+import { ComponentContent } from "./types/ComponentContent";
+import { ContentActions } from "./types/ContentActions";
 import { TakDownloadModalContent } from "./types/TakDownloadModalContent";
 
 export interface OpenTakDownloadModalCallback {
@@ -15,6 +18,20 @@ export interface RendererContext {
     readonly navigate          : NavigateFunction;
     readonly openDownloadModal : OpenTakDownloadModalCallback;
     readonly contentService    : ProductContentService;
+    readonly contentActions    : ContentActions;
+
+    // Defines parent component data, if any
+    readonly componentContent  : ComponentContent | undefined;
+
+    // Defines parent state data, if any
+    readonly stateContent  : BaseContent | undefined;
+
+    createComponentContext (
+        componentContent : ComponentContent,
+        stateContent : BaseContent,
+    ) : RendererContext;
+
+    createContextWithoutParent () : RendererContext;
 }
 
 export class RendererContextImpl implements RendererContext {
@@ -23,30 +40,82 @@ export class RendererContextImpl implements RendererContext {
     public readonly navigate          : NavigateFunction;
     public readonly openDownloadModal : OpenTakDownloadModalCallback;
     public readonly contentService    : ProductContentService;
+    public readonly contentActions    : ContentActions;
+    public readonly componentContent  : ComponentContent | undefined;
+    public readonly stateContent      : BaseContent | undefined;
 
     protected constructor (
-        t : TranslationFunction,
-        navigate : NavigateFunction,
+        t                 : TranslationFunction,
+        navigate          : NavigateFunction,
         openDownloadModal : OpenTakDownloadModalCallback,
-        contentService : ProductContentService,
+        contentService    : ProductContentService,
+        contentActions   ?: ContentActions,
+        componentContent ?: ComponentContent,
+        stateContent     ?: BaseContent,
     ) {
         this.t = t;
         this.navigate = navigate;
         this.openDownloadModal = openDownloadModal;
         this.contentService = contentService;
+        this.contentActions = contentActions ?? {};
+        this.componentContent = componentContent ?? undefined;
+        this.stateContent = stateContent ?? undefined;
     }
 
     public static create (
-        t : TranslationFunction,
+        t                 : TranslationFunction,
         navigate          : NavigateFunction,
         openDownloadModal : OpenTakDownloadModalCallback,
         contentService    : ProductContentService,
+        contentActions   ?: ContentActions,
+        componentContent ?: ComponentContent,
+        stateContent     ?: BaseContent,
     ) : RendererContextImpl {
         return new RendererContextImpl(
             t,
             navigate,
             openDownloadModal,
             contentService,
+            contentActions,
+            componentContent,
+            stateContent,
+        );
+    }
+
+    /**
+     * Creates a new component context from previous context object.
+     *
+     * @param componentContent
+     * @param stateContent
+     */
+    public createComponentContext (
+        componentContent : ComponentContent,
+        stateContent     : BaseContent,
+    ) : RendererContext {
+        return new RendererContextImpl(
+            this.t,
+            this.navigate,
+            this.openDownloadModal,
+            this.contentService,
+            this.contentActions,
+            componentContent,
+            stateContent,
+        );
+    }
+
+    /**
+     * Creates a new component context from previous context object.
+     *
+     */
+    public createContextWithoutParent () : RendererContext {
+        return new RendererContextImpl(
+            this.t,
+            this.navigate,
+            this.openDownloadModal,
+            this.contentService,
+            this.contentActions,
+            undefined,
+            undefined,
         );
     }
 
--- src/views/servicetak/quickstartguide/ServiceTakQuickstartView.tsx
+++ src/views/servicetak/quickstartguide/ServiceTakQuickstartView.tsx
@@ -1,60 +1,11 @@
 import { useEffect } from "react";
-import android from "../../../assets/icons/android.svg";
-import sanla from "../../../assets/heroimages/rakennettu3.jpeg";
-import apple from "../../../assets/icons/apple.svg";
-import windows from "../../../assets/icons/windows.svg";
-import { Layout } from "../../../components/Layout";
-import { Card } from "../../../components/Card";
-import { CardsContainer } from "../../../components/CardsContainer";
-import { ServiceInfoCard } from "../../../components/ServiceInfoCard";
-import { Trans, useTranslation } from "react-i18next";
+import { ServiceView } from "../../products/ServiceView";
 
 export function ServiceTakQuickstartView() {
-  const { t } = useTranslation();
-
   useEffect(() => {
     window.scrollTo(0, 60);
   }, []);
-
   return (
-    <div className="pb-10">
-      <Layout
-        showNavbar={true}
-        showFooter={true}
-        heroImage={sanla}
-        navbarTitle={t("serviceTakQuickstartView.navbarTitle")}
-        backUrl="/"
-      >
-        <div>
-          <CardsContainer>
-            <ServiceInfoCard
-              title={t("serviceTakQuickstartView.serviceInfoCardTitle")}
-              details={
-                <Trans
-                  i18nKey="serviceTakQuickstartView.serviceInfoCardDetails"
-                  components={{ strong: <strong />, span: <span /> }}
-                />
-              }
-            />
-
-            <Card
-              title={t("serviceTakQuickstartView.androidTitle")}
-              image={android}
-              url="/app/services/tak/quickstart/android1"
-            />
-            <Card
-              title={t("serviceTakQuickstartView.iosTitle")}
-              image={apple}
-              url="/app/services/tak/quickstart/ios1"
-            />
-            <Card
-              title={t("serviceTakQuickstartView.windowsTitle")}
-              image={windows}
-              url="/app/services/tak/quickstart/win1"
-            />
-          </CardsContainer>
-        </div>
-      </Layout>
-    </div>
+      <ServiceView name={'ServiceTakQuickstart'} />
   );
 }
