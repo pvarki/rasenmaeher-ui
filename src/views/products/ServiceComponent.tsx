38c0aa3642cfb26e0b3b617f96f86078732a704a
Jaakko Heusala <jhh@hg.fi>
2024-11-22 18:00:37 +0200

    Refactored to use newer Rune implementation https://github.com/pvarki/rasenmaeher-ui/issues/10

--- src/views/products/ServiceComponent.tsx
+++ src/views/products/ServiceComponent.tsx
@@ -8,29 +8,27 @@ import {
     RendererContext,
     RendererContextImpl,
 } from "./RendererContextImpl";
-import { ContentActions } from "./types/ContentActions";
 import { ContentType } from "./types/ContentType";
 
-export interface ServiceViewProps {
+export interface ServiceComponentProps {
     readonly name : string;
-    readonly actions ?: ContentActions;
+    readonly children ?: Node;
 }
 
-export function ServiceView ( props: ServiceViewProps ) {
+export function ServiceComponent ( props: ServiceComponentProps) {
     const {t} = useTranslation("productContent");
-    const viewName = props.name;
-    const actions = props.actions;
+    const componentName = props.name;
     const navigate = useNavigate();
-    let view = CONTENT_SERVICE.getView( viewName );
-    if ( !view ) {
-        // TODO: Implement 404 error view
-        console.warn( `Warning! Could not find view named: ${viewName}` );
-        view = {name: viewName, type: ContentType.VIEW, body: `View not found: ${viewName}`};
+    let component = CONTENT_SERVICE.getComponent( componentName );
+    if ( !component ) {
+        // TODO: Implement 404 error component
+        console.warn( `Warning! Could not find component named: ${componentName}` );
+        component = {name: componentName, type: ContentType.COMPONENT, body: `Component not found: ${componentName}`};
     }
     const { openDownloadModal, loading } = useDownloadTakZipModal();
     if ( loading ) {
         return <LoadingComponent text={ 'loading' } />;
     }
-    const context : RendererContext = RendererContextImpl.create(t, navigate, openDownloadModal, CONTENT_SERVICE, actions);
-    return ProductContentRenderer.render( view, context );
+    const context : RendererContext = RendererContextImpl.create(t, navigate, openDownloadModal, CONTENT_SERVICE);
+    return ProductContentRenderer.render( component, context );
 }
