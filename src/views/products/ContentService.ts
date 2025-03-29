a4ea50dd183a13fcde30ecc99f623f97cfe413c4
Jaakko Heusala <jheusala@iki.fi>
2025-03-29 13:37:02 +0200

    Removed rune from product list

--- src/views/products/ContentService.ts
+++ src/views/products/ContentService.ts
@@ -1,4 +1,3 @@
-import RUNE_CONTENT from "./data/rune/docs/getting-started/getting-started.json";
 import LIVELOG_CONTENT from "./data/livelog.json";
 import TAK_CONTENT from "./data/tak.json";
 import { DownloadOptionsServiceImpl } from "./DownloadOptionsService";
@@ -8,18 +7,10 @@ import {
 } from "./ProductContentService";
 import { Content } from "./types/Content";
 
-const RUNE_SERVICE_NAME = 'rune';
 const TAK_SERVICE_NAME = 'tak';
 const LIVELOG_SERVICE_NAME = 'livelog';
 
 const EMPTY_CONTENT_SERVICE : ProductContentService = ProductContentServiceImpl.create('__empty', [] );
-
-const RUNE_CONTENT_SERVICE : ProductContentService = ProductContentServiceImpl.create(
-    RUNE_SERVICE_NAME,
-    [
-        ...RUNE_CONTENT as Content[],
-    ]
-);
 
 const TAK_CONTENT_SERVICE : ProductContentService = ProductContentServiceImpl.create(
     TAK_SERVICE_NAME,
@@ -42,8 +33,7 @@ export class ContentService {
     public static getContentServiceNames () : readonly string[] {
         return [
             TAK_SERVICE_NAME,
-            LIVELOG_SERVICE_NAME,
-            RUNE_SERVICE_NAME
+            LIVELOG_SERVICE_NAME
         ];
     }
 
@@ -51,7 +41,6 @@ export class ContentService {
         switch (name) {
             case TAK_SERVICE_NAME: return TAK_CONTENT_SERVICE;
             case LIVELOG_SERVICE_NAME: return LIVELOG_CONTENT_SERVICE;
-            case RUNE_SERVICE_NAME: return RUNE_CONTENT_SERVICE;
             default : return EMPTY_CONTENT_SERVICE;
         }
     }
