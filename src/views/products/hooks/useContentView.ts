665ed90ffeebf5932753f0a690e69078bc8acdae
Jaakko Heusala <jheusala@iki.fi>
2025-03-30 02:37:50 +0200

    Improved error messages for incorrect content

--- src/views/products/hooks/useContentView.ts
+++ src/views/products/hooks/useContentView.ts
@@ -57,26 +57,102 @@ export function useContentView (serviceName : string, viewName : string) {
   return view;
 }
 
-function createProductNotFound (viewName: string) : ViewContent {
-  return {
-    name: viewName,
-    type: ContentType.VIEW,
-    body: `Product not found: ${viewName}`
-  };
+function createLoadingView (viewName: string) : ViewContent {
+  return createCardView(viewName, `Loading: ${viewName}`, "Please wait while we load the content.");
 }
 
-function createLoadingView (viewName: string) : ViewContent {
-  return {
-    name: viewName,
-    type: ContentType.VIEW,
-    body: `Loading: ${viewName}`
-  };
+function createProductNotFound (viewName: string) : ViewContent {
+  return createCardView(viewName, `${viewName}`, "The product you are looking for could not be found. Please try again later.");
 }
 
 function createViewNotFound (viewName: string) : ViewContent {
+  return createCardView(viewName, `${viewName}`, "The product you are looking for could not be found. Please try again later.");
+}
+
+function createCardView (
+  viewName: string,
+  title: string,
+  message: string,
+) : ViewContent {
   return {
     name: viewName,
     type: ContentType.VIEW,
-    body: `View not found: ${viewName}`
+    body: [
+      {
+        type: ContentType.DIV,
+        classes: [
+          "cards-container",
+          "flex",
+          "flex-col",
+          "items-center",
+          "justify-center",
+          "mx-2",
+          "h-[calc(100vh",
+          "-",
+          "4rem)]",
+          "md:px-[20%]",
+          "lg:px-[32%]",
+        ],
+        body: [
+          {
+            type: ContentType.DIV,
+            classes: [
+              "flex",
+              "flex-col",
+              "items-center",
+              "justify-center",
+              "bg-backgroundLight",
+              "rounded-lg",
+              "w-full",
+              "max-w-full",
+              "mb-5",
+              "overflow-hidden",
+              "prose",
+              "prose-white",
+            ],
+            body: [
+              {
+                type: ContentType.DIV,
+                classes: [
+                  "flex",
+                  "flex-col",
+                  "items-center",
+                  "justify-center",
+                  "bg-backgroundLight",
+                  "rounded-lg",
+                  "w-full",
+                  "max-w-full",
+                  "mb-5",
+                  "overflow-hidden",
+                  "prose",
+                  "prose-white",
+                ],
+                body: [
+                  {
+                    type: ContentType.H3,
+                    classes: [
+                      "text-xl",
+                      "font-bold",
+                      "mt-1",
+                      "mb-1"
+                    ],
+                    body: title,
+                  },
+                  {
+                    type: ContentType.P,
+                    classes: [
+                      "text-center",
+                      "ml-2",
+                      "mr-2",
+                    ],
+                    body: message
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
   };
 }
