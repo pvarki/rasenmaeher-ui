2de7af1c1853c938bb55f678636e7dba49858663
Jaakko Heusala <jheusala@iki.fi>
2025-09-09 22:49:58 +0300

    Fixed typo

--- src/views/products/dto/ProductDTO.ts
+++ src/views/products/dto/ProductDTO.ts
@@ -22,6 +22,6 @@ export function isProductDTO(obj: unknown): obj is ProductDTO {
     typeof obj.callsign === "string" &&
     "language" in obj &&
     typeof obj.language === "string" &&
-    instructions in obj
+    "instructions" in obj
   );
 }
